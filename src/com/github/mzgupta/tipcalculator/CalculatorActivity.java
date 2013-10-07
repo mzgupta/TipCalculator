6a12a9556b570bae6ee17d91fd8174ab824a963e
MaYanK <mayankgu@yahoo-inc.com>
2013-10-07 09:36:16 -0700

    default value zero to amount

--- src/com/github/mzgupta/tipcalculator/CalculatorActivity.java
+++ src/com/github/mzgupta/tipcalculator/CalculatorActivity.java
@@ -1,9 +1,11 @@
 package com.github.mzgupta.tipcalculator;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,20 +26,8 @@ public class CalculatorActivity extends Activity {
 	}
 
 	public void calculateTip (View v) {
-		float tipFraction=0F;
-		switch (v.getId()) {
-		case R.id.btnTip10:
-			tipFraction = 0.1F;
-			break;
-		case R.id.btnTip15:
-			tipFraction = 0.15F;
-			break;
-		case R.id.btnTip20:
-			tipFraction = 0.2F;			
-			break;
-		default:
-			break;
-		}
+		float tipFraction=getTipFraction(v.getId());
+		
 		EditText etAmountText = (EditText) findViewById(R.id.etAmount);
 		
 		Float amount = Float.parseFloat(etAmountText.getText().toString());
@@ -51,5 +41,34 @@ public class CalculatorActivity extends Activity {
 		
 		tipAmountView.setText("Tip is:       $"+tipAmount);
 		
+	}
+
+	private float getTipFraction(int id) {
+		float tipFraction=0F;
+		//resetColor();
+		switch (id) {
+		case R.id.btnTip10:
+			tipFraction = 0.1F;
+			break;
+		case R.id.btnTip15:
+			tipFraction = 0.15F;
+			break;
+		case R.id.btnTip20:
+			tipFraction = 0.2F;			
+			break;
+		default:
+			break;
+		}
+		return tipFraction;
+	}
+
+	private void resetColor() {
+		Button btnTip10 = (Button) findViewById(R.id.btnTip10);
+		Button btnTip15 = (Button) findViewById(R.id.btnTip15);
+		Button btnTip20 = (Button) findViewById(R.id.btnTip20);
+		
+		btnTip10.setBackgroundColor(Color.BLUE);
+		btnTip15.setBackgroundColor(Color.BLUE);
+		btnTip20.setBackgroundColor(Color.BLUE);
 	}
 }
